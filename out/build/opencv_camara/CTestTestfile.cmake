84a9185de7026956a6e3fe81a67958840f87b0c5
Minerisho <migueplata5@gmail.com>
2025-03-16 17:32:15 -0500

    Primer Commit: primera versión para testear cámara en lab

---
+++ out/build/opencv_camara/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Software/CPP/Imagenes_Aux
+# Build directory: C:/Software/CPP/Imagenes_Aux/out/build/opencv_camara
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
