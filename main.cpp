fb1d9eba7b88b92c04296b18154ed9fa0cedee6b
Minerisho <migueplata5@gmail.com>
2025-03-31 09:42:27 -0500

    fix/feat: Carpeta de RELEASES creado para versiones portátiles de la apliación, falta añadir demás dll's. Se corrige detalles en main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
-// main.cpp
-#include "mainwindow.h" // Incluye la definición de tu ventana principal
-#include <QApplication> // Incluye la clase base de la aplicación Qt
+#include "mainwindow.h" 
+#include <QApplication> 
 
 int main(int argc, char *argv[])
 {
-    // 1. Crea el objeto QApplication. Necesario para cualquier aplicación Qt GUI.
-    //    Gestiona recursos globales, el bucle de eventos, argumentos de línea de comandos, etc.
     QApplication app(argc, argv);
-
-    // 2. Crea una instancia de tu ventana principal.
     MainWindow mainWindow;
-
-    // 3. Muestra la ventana principal.
     mainWindow.show();
-
-    // 4. Inicia el bucle de eventos de Qt.
-    //    La aplicación esperará aquí eventos (clics, teclas, timeouts de timers)
-    //    y los despachará a los objetos correspondientes (ventanas, widgets).
-    //    La función exec() devuelve un código de salida cuando la aplicación termina
-    //    (usualmente cuando se cierra la última ventana o se llama a QApplication::quit()).
     return app.exec();
 }
